50c79ca5f87f73230a519adbd0c439a640312dad
Nikki <smilesandtea@gmail.com>
2017-08-08 12:46:18 -0400

    Creating a function that finds the longest string

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,16 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+let longest = " ";
+const strings = ['it', 'is', 'time', 'for', 'pizza', 'and', 'coding'];
+
+const longestString = (strings) => {
+  for (let i = 0; i < strings.length; i++){
+    if (strings[i].length > longest.length) {
+      longest = strings[i];
+    }
+  }
+  return longest;
+};
+
+console.log(longestString(strings));
