5598dff02088c6a371c9f8a19f9bf235c8f45c86
Roland Canuto <rolandcanuto@outlook.com>
2017-08-08 09:46:36 -0700

    longestString\longestString.js

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,21 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+
+console.log('Picking an OS with the highest number of characters');
+const stringLong = (strings) => {
+  let os = ['MacOSX', 'Linux', 'Windows10'];
+  console.log(os);
+    for(let i = 0; i < os.length; i++){
+      let number = os[i];
+      console.log(number.length);
+      if(number.length > 8){
+
+        console.log('Windows 10 is best OS');
+        break;
+      }//if
+    }//for
+};
+
+
+stringLong();
