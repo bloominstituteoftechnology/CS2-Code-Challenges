a762ea367c86ab4c14fa1cdeba518c609e3b54d7
phytertek <ryan.phytertek@gmail.com>
2017-08-08 12:33:53 -0400

    longestString challenge complete

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,41 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+
+// Test Cases
+/**
+ * ['Loooong', 'short', 'abcde'] => 'Loooong'
+ * ['AAAAAA','BBBB', 'CCCCC', 'DDDDDD'] => 'AAAAAA'
+ * [] => ''
+ */
+
+// Psuedo Code
+/**
+* function longestString ([strings])
+*   variable longestStringSoFar equals blank
+*   iterate over array of strings
+*     if current string is longer than longestStringSoFar
+*       set longestStringSoFar as current string
+*   return longestStringSoFar
+*/
+
+// Solution
+
+const longestString = (strings) => {
+  let longestStringSoFar = '';
+  strings.forEach(string => {
+    if (string.length > longestStringSoFar.length) {
+      longestStringSoFar = string;
+    }
+  });
+  return longestStringSoFar;
+}
+
+// Tests
+const test1 = ['Loooong', 'short', 'abcde'];
+const test2 = ['AAAAAA','BBBB', 'CCCCC', 'DDDDDD']
+const test3 = []
+
+console.log(longestString(test1) === 'Loooong')
+console.log(longestString(test2) === 'AAAAAA')
+console.log(longestString(test3) === '')
