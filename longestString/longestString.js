87000a347463f9ac91f39c065604505550243a1c
Frank Faustino <nawzyah@gmail.com>
2017-08-08 09:51:38 -0700

    Completed

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -1,4 +1,37 @@
-/*
+/* 0. Instructions
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+
+/* 1. Identify Test Cases
+    const a = ['abc', 'a', 'b'] // -> 'abc'
+    const b = ['abc', 'def'] // -> 'def'
+    const c = [] // -> null
+    const d = ['asdfasfas', 'sdfadfsa', 'asfasfasdfasfasfd', 's'] // -> 'asfasfasdfasfasfd'
+*/
+
+/* 2. Pseudo-Code
+function longestString strings
+  loop over the array of strings
+    do something in the loop
+  return longestString
+*/
+
+/* 3a. Solution #1
+const longestStr = (arr) => {
+  let x = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > x.length) x = arr[i];
+  }
+  return x;
+};
+*/
+
+// 3b. Solution #4
+const longestStr = (arr) => {
+  let x = '';
+  arr.forEach(i => {
+    if (i.length > x.length) x = i;
+  });
+  return x;
+}
