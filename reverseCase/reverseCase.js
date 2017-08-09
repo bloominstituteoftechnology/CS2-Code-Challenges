858cd1e3daba1312c9c2eaadc6e078238cc6b99e
Joram Clervius <clervius@gmail.com>
2017-08-09 12:21:51 -0400

    Problem solved but the code is long.

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -2,4 +2,28 @@
  * Write a function that reverses the case of each letter in the strings that it receives.
  * Example: 'Hello World' -> 'hELLO wORLD'
  * Assume that each string will contain only spaces and letters.
- */
+*/
+
+const changeCases = (string) => {
+	let newString = '';
+	for (let i = 0; i < string.length; i++) {
+
+		if (string[i] === ' ') {
+			newString += string[i];
+		} else {
+			if(string[i] === string[i].toUpperCase()){
+				newString += string[i].toLowerCase();
+			} else if( string[i] === string[i].toLowerCase()) {
+				newString += string[i].toUpperCase();
+				
+			} 
+		}
+		
+	}
+	console.log(newString);
+	return newString;
+}
+
+changeCases('Testing This String');
+changeCases('yOu ThInk yOu Soo SMUUrt!');
+changeCases(' hail hydra');
