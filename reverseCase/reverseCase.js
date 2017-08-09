4b88120de24f632a411308abf3c62987acaaea6e
Roland Canuto <rolandcanuto@outlook.com>
2017-08-09 09:35:46 -0700

    reverseCase\reverseCase.js

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -3,3 +3,20 @@
  * Example: 'Hello World' -> 'hELLO wORLD'
  * Assume that each string will contain only spaces and letters.
  */
+
+const caseChange = (str) =>{
+    var newCase = '';
+    for (let i = 0; i < str.length; i++){
+        if(str[i] === str[i].toLowerCase()){
+            newCase += str[i].toUpperCase();
+        }else {
+            newCase += str[i].toLowerCase();
+        }
+    }
+    console.log(newCase);
+    return newCase;
+}
+
+let string = 'Hello World';
+
+let reversedString = caseChange(string); // "sO, TODAY WE HAVE really GOOD DAY"
