30c547f44e1a0aca90f17ceac511dc7461c4e784
Allan Tito <allanv2k@hotmail.com>
2017-08-09 14:14:42 -0400

    Code Challenge commit 9UG

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -3,3 +3,15 @@
  * Example: 'Hello World' -> 'hELLO wORLD'
  * Assume that each string will contain only spaces and letters.
  */
+ const swapCase = (word) => {
+ 	const swap = {
+ 		for (let i=0; i < word.length; i++) {
+ 			const str = word.charAt(i);
+ 			swap += (str == str.toUpperCase() ? str.toLowerCase() : str.toUpperCase());
+ 			// console.log(str);
+ 		}
+ 	}
+ 	return swap;
+ };
+ const swapper = swapCase('Hello');
+ console.log(swapper);
