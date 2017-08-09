172242fcce0a69064330b55dd6e7040509cc6128
Tyler <blackbelt@yahoo.com>
2017-08-09 12:24:16 -0400

    challenge 1 and 2 completed, forgot to submit yesterday

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -3,3 +3,29 @@
  * Example: 'Hello World' -> 'hELLO wORLD'
  * Assume that each string will contain only spaces and letters.
  */
+
+ /*
+Need to iterate over every character in the string
+If it is not a letter of the alphabet (if it is a space), skip it
+If it is uppercase, make it lowercase
+If it is lowercase, make it uppercase
+New empty string -> add iterated, changed letters to empty string
+ */
+
+ const reverseCase = (str) => {
+   let str2 = '';
+   for (let i = 0; i < str.length; i++) {
+     if (str.charAt(i) === '') {
+       continue;
+     } else if (str.charAt(i) === str.charAt(i).toUpperCase()) {
+       str2 += str.charAt(i).toLowerCase();
+     } else if (str.charAt(i) === str.charAt(i).toLowerCase()) {
+       str2 += str.charAt(i).toUpperCase();
+     }
+   }
+   return str2;
+ }
+
+console.log(reverseCase("HeLlO"));
+console.log(reverseCase("I am JuST A litTlE SeNTencE."));
+console.log(reverseCase("tHIS iz guD PraKTiS"));
