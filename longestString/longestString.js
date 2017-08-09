9f01055f10066fbe6234e86ea16e323e7af89f2c
EricB <ericblancas23@gmail.com>
2017-08-09 08:57:56 -0700

    challenge

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -1,4 +1,18 @@
 /*
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
- */
+ */  
+const arr = ['cat', 'Dog', 
+           'parrot'];
+
+const lgth = 0;
+let longest;
+
+for(let i=0; i < arr.length; i++){
+    if(arr[i].length > lgth){
+        const lgth = arr[i].length;
+        longest = arr[i];
+    }      
+} 
+
+console.log(longest);
