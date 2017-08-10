d4c2b6927b6f7b8e0c7a8d187f2eb75093ea4dda
Roland Canuto <rolandcanuto@outlook.com>
2017-08-10 10:34:56 -0700

    isUnique\isUnique.js

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -3,12 +3,29 @@
  */
 
 
+ let isUnique = string => {
+   unique: 'Hello'
+   unique1: 'Hi'
+   unique2: 'This'
+ };
 
-
- const isUnique = (`!`, `@`, `#`, `$`, `%`);
-
- if (isUnique < 100) {
+ /*if (isUnique < 100) {
    console.log('this does not have unique char');
  } else
    console.log('This has unique char');
-   console.log(isUnique);
+   console.log(isUnique);*/
+
+switch (isUnique()) {
+  case (isUnique.unique): {
+    console.log('char hello');
+  }
+  case(isUnique.unique1): {
+    console.log('char hi');
+  }
+  case(isUnique.unique2): {
+    console.log('char this');
+    break;
+  }
+  console.log('No other char');
+}
+console.log('Program ending');
