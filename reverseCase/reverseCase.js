f699df384afcac6cdfbbf327e013c4e196ef99b4
Raymond <nomyards@gmail.com>
2017-08-10 10:20:22 -0400

    Complete Reverse Case

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -3,3 +3,18 @@
  * Example: 'Hello World' -> 'hELLO wORLD'
  * Assume that each string will contain only spaces and letters.
  */
+
+const stringChange = (string)=>{
+ let newString ='';
+  for (let i = 0; i < string.length; i++){
+    if(string[i] === string[i].toUpperCase()){
+      newString += string[i].toLowerCase();
+    } else {
+      newString += string[i].toUpperCase();
+    }
+   
+ }
+  console.log(newString); 
+};
+
+(stringChange('rAYMOND Rosario'));
