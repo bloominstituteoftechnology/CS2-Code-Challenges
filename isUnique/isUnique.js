52fbbe20b80d7e2e032ba5cd35fc42747c1bd96d
sam-crabtree <thedeadandroid@gmail.com>
2017-08-10 11:12:29 -0500

    isUnique complete

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -1,3 +1,16 @@
 /* Implement an algorithm to determine if a string has all unique characters.
  * What if you cannot use additional data structures?
  */
+
+const findUnique = (str) => {
+	let unique='';
+
+	for (let i = 0; i < str.length; i++) {
+		if(unique.indexOf(str[i]) === -1) {
+			unique += str[i];
+		}
+	}
+	return unique;
+ };
+
+ console.log(findUnique('This is a string'));
