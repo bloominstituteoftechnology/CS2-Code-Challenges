32d3597098def757c1bc4ba9c09acf8eaa12e394
sam-crabtree <thedeadandroid@gmail.com>
2017-08-11 12:27:44 -0500

    Redid isUnique due to misunderstanding

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -2,15 +2,27 @@
  * What if you cannot use additional data structures?
  */
 
-const findUnique = (str) => {
-	let unique='';
+// const findUnique = (str) => {
+// 	let unique='';
 
-	for (let i = 0; i < str.length; i++) {
-		if(unique.indexOf(str[i]) === -1) {
-			unique += str[i];
-		}
-	}
-	return unique;
+// 	for (let i = 0; i < str.length; i++) {
+// 		if(unique.indexOf(str[i]) === -1) {
+// 			unique += str[i];
+// 		}
+// 	}
+// 	return unique;
+//  };
+
+//  console.log(findUnique('This is a string'));
+
+// ^^^ old code because i misunderstood the assignment, lol
+
+
+ const isUnique = (str) => {
+	 for (let i = 0; i < str.length; i++) {
+		 for (let j = i +1; j < str.length; j++) {
+			 if (str[i] == str[j]) return false;
+		 }
+	 }
+		 return true;
  };
-
- console.log(findUnique('This is a string'));
