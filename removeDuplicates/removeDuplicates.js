1aacd4c2b32fc15f68aa9614da1c3311ae9744b8
Frank Faustino <frank@Franks-MacBook-Pro.local>
2017-08-14 09:56:38 -0700

    Completed removeDuplicates.js

--- removeDuplicates/removeDuplicates.js
+++ removeDuplicates/removeDuplicates.js
@@ -12,13 +12,7 @@
 const a = [1, 1, 1, 2, 2, 3, 4, 5, 5] // -> [1, 2, 3, 4, 5]
 const b = ["spray", "spray", "limit", "elite", "elite", "exuberant", "destruction", "present"]
 
-/* 2. Pseudo-Code
-	initialize array with first element
-	loop thrugh arr
-		compare second element with first element
- */
-
-// 3a. Solution #1 using for loop
+// 2a. Solution #1 using for loop
 const removeDuplicates = (arr) => {
 	let seen = {};
 	let result = [];
@@ -35,7 +29,7 @@ const removeDuplicates = (arr) => {
 console.log(removeDuplicates(a))
 console.log(removeDuplicates(b))
 
-// 3b. Solution #1
+// 2b. Solution #1
 // const removeDuplicates = (arr) => {
 // 	return arr.filter((element, index, array) => { // filter creates new array from elements that return true
 // 		return !index || element != array[index - 1];
@@ -45,7 +39,7 @@ console.log(removeDuplicates(b))
 // console.log(removeDuplicates(a))
 // console.log(removeDuplicates(b))
 
-// 3c. Solution #3 using new Set();
+// 2c. Solution #3 using new Set();
 // const removeDuplicates = (arr) => Array.from(new Set(arr));
 
 // console.log(removeDuplicates(a))
