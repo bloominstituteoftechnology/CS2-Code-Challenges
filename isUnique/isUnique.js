d517fd6a5c8b19a28a5a0f81ee957632abf56c30
Roland Canuto <rolandcanuto@outlook.com>
2017-08-15 09:14:28 -0700

    brainTeasers\waterJugs.md

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -2,18 +2,18 @@
  * What if you cannot use additional data structures?
  */
 
-
+/*
  let isUnique = string => {
    unique: 'Hello'
    unique1: 'Hi'
    unique2: 'This'
  };
 
- /*if (isUnique < 100) {
+ if (isUnique < 100) {
    console.log('this does not have unique char');
  } else
    console.log('This has unique char');
-   console.log(isUnique);*/
+   console.log(isUnique);
 
 switch (isUnique()) {
   case (isUnique.unique): {
@@ -29,3 +29,47 @@ switch (isUnique()) {
   console.log('No other char');
 }
 console.log('Program ending');
+
+*/
+
+/*
+function isUnique structures
+iterate over str
+iterate over str starting at i + 1
+if we find a match return false
+return true;
+*/
+
+/*
+const isUnique = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+        if (str[i] === str[j]) return false;
+    }
+  }
+  return true;
+};
+const str1 = 'abcdefg';
+const str2 = 'abcdefgg';
+const result = isUnique(str1);
+console.log(result);
+*/
+
+
+
+const isUnique = (str) => {
+  const set = new Set();
+  for (let i = 0; i < str.length; i++) {
+    const character = str[i];
+    const isInSet = set.has(character);
+    if(isInSet) return false;
+    if (!isInSet) set.add(character);
+  }
+  return true;
+};
+const str1 = '1234567890qwertyuiopasdfghjklzxcvbnm';
+const str2 = 'abcdefgg';
+
+const result = isUnique(str1);
+
+console.log(result);
--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,7 +2,7 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
-
+/*
 console.log('Picking an OS with the highest number of characters');
 const stringLong = (strings) => {
   let os = ['MacOSX', 'Linux', 'Windows10'];
@@ -21,3 +21,15 @@ const stringLong = (strings) => {
 
 
 stringLong();
+*/
+
+
+
+const reverseCase = (string) =>
+string
+      .split('')
+      .map(c => c.toUpperCase() === c ? c.toLowerCase() : c.toUpperCase())
+      .join('')
+
+
+console.log(reverseCase('AHSDHWADHShsdahdhwhadhwahdHHDAWHDASHDA'));
