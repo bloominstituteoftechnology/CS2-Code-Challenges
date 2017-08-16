dfdff897e10b5a343cbd9a6e63c60bc4d87a3a13
Frank Faustino <frank@Franks-MacBook-Pro.local>
2017-08-16 09:54:45 -0700

    Completed forLoopTimeout.js

--- forLoopTimeout/forLoopTimeout.js
+++ forLoopTimeout/forLoopTimeout.js
@@ -5,19 +5,15 @@
 // The output should be 1, 2, 3, .... 10. Right now it just prints 11.
 // I've been asked this three times in separate interviews.
 
-// 2a. let instead of var (ES6 fix)
+// 2a. Solution: let instead of var (ES6 fix)
 // for (let i = 1; i <= 10; i++) {
 // 	setTimeout(() => console.log(i), 0);
-		// From looking at the code you would assume it would print 1 - 10
-		// It doesn't.  Why?  How can you make it print 1 - 10.
 // }
 
-// 2b. setting setTimeout scope
+// 2b. Solution: setting setTimeout scope
 function x(i) {
 	setTimeout(() => console.log(i), 0)
 }
-		// From looking at the code you would assume it would print 1 - 10
-		// It doesn't.  Why?  How can you make it print 1 - 10.
 for (var i = 1; i <= 10; i++) {
 	x(i);
 }
