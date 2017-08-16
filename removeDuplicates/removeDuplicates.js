00bff0b05518c078c44cd5d37e107eb407467161
cmontoya77 <cmontoya77@att.net>
2017-08-16 12:53:27 -0400

    CS2 Code Challenge updated

--- removeDuplicates/removeDuplicates.js
+++ removeDuplicates/removeDuplicates.js
@@ -7,7 +7,7 @@
  *  So if you're using 'indexOf' 'sort' 'forEach' etc, 
  *  you're more than likely using a for loop under the hood.
  */
-var arr = [1, 1, 1, 2, 2, 3, 4, 5, 5];
+var arr = [1, 1, 1, 2, 2, 3, 4, 5, 5, 6];
 function newNum(arr){
     let newArr = [];
     for(var i = 0; i < arr.length; i++){
@@ -21,4 +21,3 @@ function newNum(arr){
 console.log(newNum(arr));
 	
 	//code here...
-};
