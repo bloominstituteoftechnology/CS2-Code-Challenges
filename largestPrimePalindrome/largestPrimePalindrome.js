1e8a8a7a95675f924421a0c1c006ab36063d5a1e
phytertek <ryan.phytertek@gmail.com>
2017-08-17 12:54:34 -0400

    var to let

--- largestPrimePalindrome/largestPrimePalindrome.js
+++ largestPrimePalindrome/largestPrimePalindrome.js
@@ -11,7 +11,7 @@ function isPrime(number) {
   if (number < 2) {return false}
   if (number != Math.round(number)) {return false}
   let isPrime = true;
-  for (var i = 2; i <= Math.sqrt(number); i++) {
+  for (let i = 2; i <= Math.sqrt(number); i++) {
     if (number % i == 0) {isPrime = false}
   }
   return isPrime;
