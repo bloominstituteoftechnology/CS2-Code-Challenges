2ae39a41636c6e11dbc30cefb26db643bcbb5b52
phytertek <ryan.phytertek@gmail.com>
2017-08-17 12:50:42 -0400

    optimization1  of largestPrimePalindrome

--- largestPrimePalindrome/largestPrimePalindrome.js
+++ largestPrimePalindrome/largestPrimePalindrome.js
@@ -7,25 +7,23 @@
 
 //Test 1000 => 929
 
-const isPrime = (number) => {
-  if (number === 1 || number === 3) {
-    return false;
+function isPrime(number) {
+     if (number < 2) {return false}
+     if (number != Math.round(number)) {return false}
+     let isPrime = true;
+     for (var i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i == 0) {isPrime = false}
+     }
+     return isPrime;
   }
-  for (let i = 2; i < number; i++) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
 
 const longestPalindromePrime = (number) => {
   for (let i = 0; i < number; i++){
     const test = number - i;
-    if (isPrime(test) && test === parseInt([...test+''].reverse().join(''))) {
+    if (isPrime(test) && test === parseInt([...test.toString()].reverse().join(''))) {
       return test
     }
   }
 }
 
-console.log(longestPalindromePrime(1000) === 929);
+console.log(longestPalindromePrime(500));
