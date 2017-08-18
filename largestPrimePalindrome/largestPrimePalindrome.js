bdfc74867d69b8308ddc3174ccb6105f07f2b425
phytertek <ryan.phytertek@gmail.com>
2017-08-18 13:48:34 -0400

    Added Frank's soulution... damn its fast!

--- largestPrimePalindrome/largestPrimePalindrome.js
+++ largestPrimePalindrome/largestPrimePalindrome.js
@@ -7,23 +7,75 @@
 
 //Test 1000 => 929
 
-function isPrime(number) {
-  if (number < 2) {return false}
-  if (number != Math.round(number)) {return false}
-  let isPrime = true;
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i == 0) {isPrime = false}
+// WAY WAYYY faster
+const largestPrimePalindrome = n => {
+  const isPrime = n => {
+    let divisor = 3,
+        limit = Math.sqrt(n);
+  
+    if (n === 2 || n === 3) return true;
+    if (n % 2 === 0) return false;
+  
+    while (divisor <= limit) {
+      if (n % divisor === 0) return false;
+      else divisor += 2;
+    }
+    return true;
   }
-  return isPrime;
+  
+  const isPalindrome = n => {
+    let str = n.toString(),
+        len = str.length;
+  
+    for (let i = 0; i < len / 2; i++) {
+      if (str[i] !== str[len -1 -i])
+        return false;
+    }
+    return true;
+  }
+
+  for (let i = n; i > 0; i--) {
+    if (isPrime(i) && isPalindrome(i)) return i;
+  }
 }
 
-const longestPalindromePrime = (number) => {
-  for (let i = 0; i < number; i++){
-    const test = number - i;
-    if (isPrime(test) && test === parseInt([...test.toString()].reverse().join(''))) {
-      return test
-    }
-  }
-}
+console.log(largestPrimePalindrome(1000));
+
+// // WAAAY Faster
+// const longestPalindromePrime = (num) => {
+//   for (let i = num; i > 0; i--) {
+//     const str = i.toString();
+//       if (str === str.split('').reverse().join('')) {
+//         let j = 2;
+//         let count = 0;
+//         while (count === 0 && j <= (i/2)) {
+//           if (i % j === 0) count++;
+//           j++;
+//         }
+//         if (count === 0) return i;
+//       }
+//   }
+// };
+
+// function isPrime(number) {
+//   if (number < 2) {return false}
+//   if (number != Math.round(number)) {return false}
+//   let isPrime = true;
+//   for (let i = 2; i <= Math.sqrt(number); i++) {
+//     if (number % i == 0) {isPrime = false}
+//   }
+//   return isPrime;
+// }
+
+// const longestPalindromePrime = (number) => {
+//   for (let i = 0; i < number; i++){
+//     const test = number - i;
+//     if (isPrime(test)) {
+//       if (test === parseInt([...test.toString()].reverse().join(''))) {
+//         return test
+//       }
+//     }
+//   }
+// }
 
 console.log(longestPalindromePrime(500));
