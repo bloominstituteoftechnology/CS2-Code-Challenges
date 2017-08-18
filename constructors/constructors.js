79ba5fc991113fb360c1e251cbbd94129ab3c386
Tyler <blackbelt@yahoo.com>
2017-08-18 12:55:53 -0400

    finished constructors.js

--- constructors/constructors.js
+++ constructors/constructors.js
@@ -21,3 +21,123 @@
  * This is how you would structure the game objects in an actual game
  * application in Unity or another similar framework.
  */
+
+ class NPC {
+    constructor (hp, strength, speed, intelligence, isFriendly, build, attackType, fight) {
+        this.hp = hp;
+        this.strength = strength;
+        this.speed = speed;
+        this.intelligence = intelligence;
+        this.isFriendly = isFriendly;
+        this.build = build;
+        this.attackType = attackType;
+        this.fight = fight;
+    }
+    attack(enemy) {
+        enemy.hp -= (Math.floor(Math.random() * 30));
+        enemy = enemy.toUpperCase();
+        console.log(`${enemy}'s hp is now at ${enemy.hp}!`);
+    }
+ }
+
+ class Humanoid extends NPC {
+    constructor (roboOrBio, speaksEnglish, speaksOther, faveWeapon) {
+        this.roboOrBio = roboOrBio;
+        this.speaksEnglish = speaksEnglish;
+        this.speaksOther = speaksOther;
+        this.faveWeapon = faveWeapon;
+    }
+ }
+
+class Animal extends NPC {
+    constructor (hasHair, isOmnivore, numLegs, hasTail) {
+        this.hasHair = hasHair;
+        this.isOmnivore = isOmnivore;
+        this.numLegs = numLegs;
+        this.hasTail = hasTail;
+    }
+}
+
+class Plant extends NPC {
+    constructor(isPoisonous, isPretty, isTasty, color) {
+        this.isPoisonous = isPoisonous;
+        this.isPretty = isPretty;
+        this.isTasty = isTasty;
+        this.color = color;
+    }
+}
+
+class Human extends Humanoid {
+    constructor (isMultilingual, isMarried, birthCity) {
+        this.isHuman = true;
+        this.isMultilingual = isMultilingual;
+        this.isMarried = isMarried;
+        this.birthCity = birthCity;
+    }
+}
+
+class Elf extends Humanoid {
+    constructor (earType, height, houseStyle) {
+        this.earType = earType;
+        this.height = height;
+        this.houseStyle = houseStyle;
+    }
+}
+
+class Orc extends Humanoid {
+    constructor (naughtyOrNice, fightingStyle) {
+        this.naughtyOrNice = naughtyOrNice;
+        this.fightingStyle = fightingStyle;
+    }
+}
+
+class Bear extends Animal {
+    constructor (wildOrTamed, cubOrMama) {
+        this.wildOrTamed = wildOrTamed;
+        this.cubOrMama = cubOrMama;
+    }
+}
+
+class Wolf extends Animal {
+    constructor (howlsAtTheMoon) {
+        this.hasFangs = true;
+        this.howlsAtTheMoon = howlsAtTheMoon;
+    }
+}
+
+class FleshEatingDaisy extends Plant {
+    constructor (name) {
+        this.isPoisonous = true;
+        this.isHorrible = true;
+        this.sucks = true;
+        this.worstFear = 'Roundup';
+        this.name = name;
+        if (this.name !== 'Tulip') {
+            this.name = 'Tulip';
+        }
+    }
+}
+
+class Soldier extends Human {
+    constructor (faveGun, camoColor) {
+        this.isAwesome = true;
+        this.faveGun = faveGun;
+        this.camoColor = camoColor;
+    }
+}
+
+class Peasant extends Soldier {
+    constructor (isHungry, isLoyal) {
+        this.isHungry = isHungry;
+        this.isLoyal = isLoyal;
+    }
+}
+
+class Bandit extends Human {
+    constructor () {
+        this.thief = true;
+    }
+    steal() {
+        console.log('The bandit robbed you and got away!');
+    }
+}
