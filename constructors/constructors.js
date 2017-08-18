3dd08568db282f89630df167ea192c228053b815
Raymond <nomyards@gmail.com>
2017-08-18 12:44:16 -0400

    complete challenge

--- constructors/constructors.js
+++ constructors/constructors.js
@@ -21,3 +21,113 @@
  * This is how you would structure the game objects in an actual game
  * application in Unity or another similar framework.
  */
+
+class NPC {
+    constructor(options) {
+        this.hp = options.hp;
+        this.strength = options.strength;
+        this.agility = options.agility;
+        this.intellect = options.intellect;
+     }
+}
+class Humanoid extends NPC {
+    constructor(options) {
+        super(options);
+
+    }
+}
+class Animal extends NPC {
+    constructor(options) {
+        super(options);
+    }
+}
+class Plant extends NPC {
+    constructor(options) {
+        super(options);
+    }
+}
+class Human extends Humanoid {
+    constructor(options) {
+        super(options);
+        this.name = options.name;
+        this.hairColor = options.hairColor;
+        this.skinColor = options.skinColor;
+        this.facialHair = options.facialHair;
+        this.height = options.height;
+    }
+}
+class Elf extends Humanoid {
+    constructor(options) {
+        super(options);
+        this.name = options.name;
+        this.hairColor = options.hairColor;
+        this.skinColor = options.skinColor;
+        this.height = options.height;
+    }
+}
+class Orc extends Humanoid {
+    constructor(options) {
+        super(options);
+        this.name = options.name;
+        this.hairColor = options.hairColor;
+        this.skinColor = options.skinColor;
+        this.scars = options.scars;
+        this.facialHair = options.facialHair;
+        this.height = options.height;
+    }
+}
+class Bear extends Animal {
+    constructor(options) {
+        super(options);
+        this.typeOfBear = options.typeOfBear;
+        this.size = options.size;
+        this.ferocity = options.ferocity;
+    }
+    
+} 
+class Wolf extends Animal {
+    constructor(options) {
+        super(options);
+        this.typeOfWolf = options.typeOfWolf;
+        this.size = options.size;
+        this.ferocity = options.ferocity;
+
+    }
+}
+class FleshEatingdaisy extends Plant {
+    constructor(options) {
+        super(options);
+        this.size = options.size;
+        this.color = options.color;
+    }
+}
+class Solider extends Human {
+    constructor(options) {
+        super(options);
+        this.tacticalMind = options.tacticalMind;
+        this.weaponMastery = options.weaponMastery;
+        this.willToLive = options.willToLive;
+    }
+}
+class Peasant extends Human {
+    constructor(options) {
+        super(options);
+        this.workEthic = options.workEthic;
+        this.barteringSkill = options.barteringSkill;
+        this.woodWorking = options.woodWorking;
+        this.fishing = options.fishing;
+        this.hunting = options.hunting;
+        this.blacksmithing = options.blacksmithing;
+    }
+}
+class Bandit extends Human {
+    constructor(options) {
+        super(options);
+        this.guile = options.guile;
+        this.archery = options.archery;
+        this.speed = options.speed;
+        this.pickPocket = options.pickPocket;
+        this.traps = options.traps;
+        this.morality = options.morality;
+    }
+}
