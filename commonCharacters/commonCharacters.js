571ce1cb4a3dc35775ca5c0816cc38c63cb43580
Roland Canuto <rolandcanuto@outlook.com>
2017-08-21 09:58:10 -0700

    complete

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -6,3 +6,28 @@
  * Example: commonCharacters('acexivou', 'aegihobu')  *
  * Returns: 'aeiou'
 */
+
+const commonChar = (str1, str2) => {
+    let string = [];
+    if (str1.indexOf('a') > -1 && str2.indexOf('a') > -1) {
+        string.push('a');
+    }
+    if (str1.indexOf('e') > -1 && str2.indexOf('e') > -1) {
+        string.push('e');
+    }
+    if (str1.indexOf('i') > -1 && str2.indexOf('i') > -1) {
+        string.push('i');
+    }
+    if (str1.indexOf('o') > -1 && str2.indexOf('o') > -1) {
+        string.push('o');
+    }
+    if (str1.indexOf('u') > -1 && str2.indexOf('u') > -1) {
+        string.push('u');
+    }
+    return string.join('');
+}
+
+const str1 = 'areyiwoqu';
+const str2 = 'asedifogu';
+
+console.log(commonChar(str1, str2));
