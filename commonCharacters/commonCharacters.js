e2f5846f473c3657a1cfe5c992ab870d80d7f7cb
sam-crabtree <thedeadandroid@gmail.com>
2017-08-21 11:49:52 -0500

    commonCharacters complete

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -1,8 +1,22 @@
 /*
  * Common Characters:
- * Write a function that accepts two strings as arguments, and returns only the characters that are common to both strings. *
- * Your function should return the common characters in the same order that they appear in the first argument.
+ * Write a function that accepts two strings as arguments, 
+ * and returns only the characters that are common to both strings. *
+ * Your function should return the common characters in the same order 
+ * that they appear in the first argument.
  * Do not return duplicate characters and ignore whitespace in your returned string.  *
  * Example: commonCharacters('acexivou', 'aegihobu')  *
  * Returns: 'aeiou'
 */
+
+const commonCharacters = function(string1, string2) {
+  let sameChars = '';
+  for (let i = 0; i < string2.length; i++) {
+    if (string1.includes(string2[i])) {
+      sameChars += string2[i];
+    } 
+  }
+  return `Identical characters: ${sameChars}`;
+};
+
+console.log(commonCharacters('acexivou', 'aegihobu'));
