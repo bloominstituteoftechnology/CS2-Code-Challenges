f9085448aebd12860e1fdb489b1bf14b49126b93
Raymond <nomyards@gmail.com>
2017-08-21 12:46:04 -0400

    refactored for es6

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -8,8 +8,8 @@
 */
 const commonChars = (string1, string2) => {
     let combinedStr = '';
-    for(var i = 0; i < string1.length; i++){
-      for(var j = 0; j < string2.length; j++){
+    for(let i = 0; i < string1.length; i++){
+      for(let j = 0; j < string2.length; j++){
           if(string1[i] === string2[j]){
             combinedStr += string1[i];
           }
