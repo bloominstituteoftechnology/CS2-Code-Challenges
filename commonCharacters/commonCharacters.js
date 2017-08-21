0884876a46c3d1b66b99fe4574b08ab8dd1ae196
codealtgeek <codealtgeek@gmail.com>
2017-08-21 12:14:02 -0400

    Common Characters Done

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -6,3 +6,20 @@
  * Example: commonCharacters('acexivou', 'aegihobu')  *
  * Returns: 'aeiou'
 */
+
+
+commonCharacter = (string1, string2) => {
+    let holdString = '';
+for(let i = 0; i < string2.length; i++){
+    if(string1.includes(string2[i]) ){
+        holdString += string2[i];
+    }
+}
+return holdString;
+
+}
+
+string1 = 'acexivouwrse';
+string2 = 'aegihobuxtrdtlv';
+
+console.log(commonCharacter(string1, string2));
