643918e7b82d8150c2dc0b444ada436d362a3d8a
Mjax <matt.d.jax@gmail.com>
2017-08-22 09:38:55 -0700

    complete evenOccurene

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -6,10 +6,26 @@
 
 /*
  *  * example usage:
- *   * const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
  *    * console.log(onlyEven); //  4
  *    */
 
 const evenOccurence = (arr) => {
-  // Your code here.
+  const occNums = {};
+  arr.forEach((x) => {
+    if (occNums[x]) occNums[x] += 1;
+    else occNums[x] = 1;
+  });
+  const occNumKeys = Object.keys(occNums);
+  let firstEvenOcc; 
+  occNumKeys.forEach((x) => {
+    if (occNums[x] % 2 === 0) {
+      if (!firstEvenOcc) firstEvenOcc = x;
+      return;
+    }
+  });
+  if (firstEvenOcc) return firstEvenOcc;
+  return null;
 };
+const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
+console.log(onlyEven);
+
