7cf26d0cd4601ad4262d42a18adcfcd907b79146
Joram Clervius Surface <clervius@gmail.com>
2017-08-22 12:30:34 -0400

    evenOccurences done.

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -12,4 +12,22 @@
 
 const evenOccurence = (arr) => {
   // Your code here.
+  const uniqueSet = Array.from(new Set(arr));
+  let counter = 0;
+  for (let i = 0; i < uniqueSet.length; i++) {
+  	counter = 0;
+  	for (let j = 0; j < arr.length; j++) {
+  		if (uniqueSet[i] === arr[j]) {
+  			counter++;
+  		}
+  	}
+  	if (counter % 2 === 0) {
+        console.log(counter);
+  		return arr[i];
+  	}
+  }
+  return null;
 };
+
+const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
+console.log(onlyEven); 
