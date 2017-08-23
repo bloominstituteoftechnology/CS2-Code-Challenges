2cb8696622fb09593c2335b8e3c0950f1c3584a7
Frank Faustino <nawzyah@gmail.com>
2017-08-23 09:20:43 -0700

    Completed stringCompression.js

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -4,3 +4,24 @@
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
+
+// Test Case
+const a = "aabcccccaaa"; // 'a2b1c5a3'
+
+// Solution
+const stringCompression = str => {
+  let a = [],
+      count = 1,
+      i;
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else if (str[i] != str[i + 1]) {
+      a.push(`${str[i]}${count}`);
+      count = 1;
+    }
+  }
+  return a.join('');
+};
+
+console.log(stringCompression(a));
