ca5a15f70ff329b1e4d3a3694b28f0ff7f063dba
codealtgeek <codealtgeek@gmail.com>
2017-08-23 12:28:10 -0400

    Done String Comp

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -4,3 +4,28 @@
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
+
+
+stringCompression = (str) => {
+    const holderArray = [];
+    let counter = 1;
+   
+    for (let i = 0; i < str.length; i++) {
+      
+      if (str[i] === str[(i + 1)]) {
+        counter++;
+      } else {
+        holderArray.push(str[i]);
+        holderArray.push(counter);
+        counter = 1;
+      }
+    }
+   
+    if (holderArray.concat('').length < str.length) {
+      return holderArray.join('');
+   }
+   return str;
+  };
+ 
+ let doneThis = stringCompression("aabcccccaaa");
+ console.log(doneThis);
