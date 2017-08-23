f312172a39a16af5257210c129839612b318dceb
Adam Lower <adamlowerj@gmail.com>
2017-08-23 13:13:02 -0400

    Finished

--- largestPrimePalindrome/largestPrimePalindrome.js
+++ largestPrimePalindrome/largestPrimePalindrome.js
@@ -4,3 +4,22 @@
  * You will first want to determine if the number is a palindrome and then determine if it is prime.
  * A palindrome is a number that is the same forwards and backwards: 121, 323, 123454321, etc.
  */
+
+const largestPrimePalindrome = () => {
+	const isPalindrome = (num) => {
+		if (num === num.toString().split('').reverse().join('')) return true;
+	} 
+
+	const isPrime = (num) => {
+		if (num % 2 !== 0) {
+			for (let j = 3; j <= Math.sqrt(num); j++) {
+				if (num % j === 0) return false;
+			}
+			return true;
+		}
+	}
+	
+	for (let i = 1000; i > 0; i--) {
+		if (isPalindrome(i) && isPrime(i)) return i;
+	 }
+  }
