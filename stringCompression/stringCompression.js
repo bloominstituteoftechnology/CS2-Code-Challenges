e864a78977d4feaba81ca1ff1ebc7caa75ca0c69
sam-crabtree <thedeadandroid@gmail.com>
2017-08-23 11:52:57 -0500

    stringCompression complete (for now)

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -4,3 +4,31 @@
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
+
+const stringCompression = function(str) {
+  let count = 1;
+  let previous = str.charAt(0);
+  let compressedStr = '';
+
+  for (let i = 1; i < str.length; i++) {
+    var current = str[i];
+
+    if (previous === current) {
+      count++;
+    } else {
+      compressedStr += previous + count;
+      previous = current;
+      count = 1;
+    }
+  }
+
+  if (count === 1) {
+    compressedStr += current;
+  } else {
+    compressedStr += current + count;
+  }
+
+  return compressedStr;
+}
+
+console.log(stringCompression('aaaabbbbcddd'));
