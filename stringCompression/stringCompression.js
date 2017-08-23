54b539c3d538e4b6103ed6069bd682fc3449b777
phytertek <ryan.phytertek@gmail.com>
2017-08-23 11:56:49 -0400

    stringCompression complete

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -4,3 +4,25 @@
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
+
+const test1 = 'aabcccccaaa' // => 'a2b1c5a3'
+const test2 = 'aaaaaaaaaaaccccccdddddddefg' // => 'a11c6d7e1f1g1'
+const test3 = 'AaaaBBbCcccDDDd'// 'AaaaBBbCcccDDDd'
+const test4 = 'ABCDD'// => 'ABCDD'
+
+const compressor = (string) => {
+  const letters = []
+  for(let i = 0; i < string.length; i++) {
+    if (i === 0 || string[i] !== string[i - 1]) {
+      letters.push(...[string[i], 1])
+    } else {
+      letters[letters.length - 1]++
+    }
+  }
+  return letters.length > string.length ? string : letters.join('')
+}
+
+console.log(compressor(test1))
+console.log(compressor(test2))
+console.log(compressor(test3))
+console.log(compressor(test4))
