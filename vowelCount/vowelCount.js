c2105a0ac6e9c20b6b2aa26bc2f126f5e0ce26df
Mjax <matt.d.jax@gmail.com>
2017-08-24 09:17:23 -0700

    complete vowel count

--- vowelCount/vowelCount.js
+++ vowelCount/vowelCount.js
@@ -2,3 +2,19 @@
  * Write a function that returns the count of the total number of vowels in a string.
  * Example: 'Hello World!' -> 3
  */
+const vowelCount = (str) => {
+  const vowels = {
+    a: true,
+    e: true,
+    i: true,
+    o: true,
+    u: true
+  };
+  let count = 0;
+  for (let i = 0; i < str.length; i++){
+    if (vowels[str[i].toLowerCase()]) count += 1;
+  } 
+  return count;
+};
+
+console.log(vowelCount('Hello WOrld!'));
