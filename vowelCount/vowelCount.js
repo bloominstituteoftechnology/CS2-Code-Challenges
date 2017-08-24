c9be8b824e2a5eb5fdea7baab0e905d1b1a311e4
phytertek <ryan.phytertek@gmail.com>
2017-08-24 12:13:20 -0400

    vowelCount challenge complete

--- vowelCount/vowelCount.js
+++ vowelCount/vowelCount.js
@@ -2,3 +2,7 @@
  * Write a function that returns the count of the total number of vowels in a string.
  * Example: 'Hello World!' -> 3
  */
+
+ const vowelCounter = str => str.match(/([aeiou])/g).length
+
+ console.log(vowelCounter('Fuuuuuk'))
