d6f1032de0ae7b569926dcfa5e3fa0c068797f5c
phytertek <ryan.phytertek@gmail.com>
2017-08-24 12:27:07 -0400

    fix vowelCount to ignore case in regex

--- vowelCount/vowelCount.js
+++ vowelCount/vowelCount.js
@@ -3,6 +3,6 @@
  * Example: 'Hello World!' -> 3
  */
 
- const vowelCounter = str => str.match(/([aeiou])/g).length
+ const vowelCounter = str => str.match(/([aeiou])/gi).length
 
- console.log(vowelCounter('Fuuuuuk'))
+ console.log(vowelCounter('FUuUuuuuuk'))
