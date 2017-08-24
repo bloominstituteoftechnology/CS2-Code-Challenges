aa8af7ec7eb05e8dc901aafcb1596b9e49525fd6
Frank Faustino <nawzyah@gmail.com>
2017-08-24 12:03:32 -0700

    Completed stringCompression.js / Added check for 'compressed' string.length

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -7,6 +7,7 @@
 
 // Test Case
 const a = "aabcccccaaa"; // 'a2b1c5a3'
+const b = "qwertyuiop"; // 'qwertyuiop'
 
 // Solution
 const stringCompression = str => {
@@ -16,12 +17,46 @@ const stringCompression = str => {
   for (i = 0; i < str.length; i++) {
     if (str[i] === str[i + 1]) {
       count++;
-    } else if (str[i] != str[i + 1]) {
+    } else if (str[i] !== str[i + 1]) {
       a.push(`${str[i]}${count}`);
       count = 1;
     }
   }
-  return a.join('');
+  // added conditional check to see if "compressed" string is longer than original string
+  const result = a.join('');
+  return result.length >= str.length ? str : result;
 };
 
 console.log(stringCompression(a));
+
+// Tai's Solution
+  // currentChar = null
+  // compressedString = ''
+  // charCount = 1
+  // use a for loop, i = 0, until string.length, i++
+    // if string.charAt(i) is equal to currentChar, charCount++
+    // if (currentChar === null, currentChar = string.charAt(i)
+    // if (string.charAt(i) !== currentChar || string.charAt(i) === undefined
+      // compressedString += currentChar;
+      // compressedString += charCount;
+      // charCount = 1;
+      // currentChar = string.charAt(i);
+  // return using ternary statement (compressedString.length > string.length ? string
+  // : compressedString;
+// const compressString = (string) => {
+//   let currentChar = string.charAt(0);
+//   let compressedString = '';
+//   let charCount = 0;
+//   for (let i = 0; i <= string.length; i++) {
+//     if (string.charAt(i) === currentChar) charCount++;
+//     if (string.charAt(i) !== currentChar || string.charAt(i) === undefined) {
+//       compressedString += currentChar;
+//       compressedString += charCount;
+//       charCount = 1;
+//       currentChar = string.charAt(i);
+//     }
+//   }
+//   return compressedString.length > string.length ? string : compressedString;
+// };
+
+// console.log(compressString('qwertyuiop'));
