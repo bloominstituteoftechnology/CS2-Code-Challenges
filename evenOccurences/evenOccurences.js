3aa2f844287a90ee875f7e28783f2db340f26047
phytertek <ryan.phytertek@gmail.com>
2017-08-24 12:12:58 -0400

    fix evenOccurance to account for ES6 Object traversal of integer keys

--- evenOccurences/evenOccurences.js
+++ evenOccurences/evenOccurences.js
@@ -12,22 +12,24 @@
 
 const evenOccurence = (arr) => {
   // Your code here.
-  const occ = {}
-  let returnVal;
+  const occurances = {}
+  let returnVal = null
   for (let i = 0; i < arr.length; i++) {
-    if (occ[arr[i]]) occ[arr[i]]++;
-    else occ[arr[i]] = 1;
+    if (occurances['0'+arr[i]]) occurances['0'+arr[i]]++;
+    else occurances['0'+arr[i]] = 1;
   }
-  const occKeys = Object.keys(occ);
-  for (let j = 0; j < occKeys.length; j++) {
-    if(occ[occKeys[j]] % 2 === 0) {
-      returnVal = occKeys[j];
+  const occuranceKeys = Object.keys(occurances);
+  for (let j = 0; j < occuranceKeys.length; j++) {
+    if(occurances[occuranceKeys[j]] % 2 === 0) {
+      returnVal = occuranceKeys[j].splice(0,1);
       break;
     }
   }
+  Reflect
   return returnVal;
 };
 
-const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
+const onlyEven = evenOccurence([1, 7, 4, 2, 5, 1, 6, 8, 9, 6, 4, 3, 2, 1]);
 console.log(onlyEven); //  4
 
+console.log(evenOccurence([1,2,3,4,5]))
