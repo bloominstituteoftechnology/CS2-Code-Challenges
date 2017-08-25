9609043b7a746b403d73e7e2a1d0b30f8fbbf9bd
Frank Faustino <nawzyah@gmail.com>
2017-08-25 00:03:03 -0700

    Completed vowelCount.js

--- vowelCount/vowelCount.js
+++ vowelCount/vowelCount.js
@@ -8,12 +8,7 @@ const a = 'Hello World!'; // 3
 const b = ''; // 0
 const c = 'Disrupt jean shorts viral hella meh, plaid cupidatat magna art party.' // 20
 
-/* Pseudo-Code
- * init vowels variable
- * 
- */
-
- // Solution using match() and RegEx
+// Solution using match() and RegEx
 // const vowelCount = str => (str.match(/[aeiou]/gi) || []).length;
 
 // Solution using Array.from(), filter() and includes()
