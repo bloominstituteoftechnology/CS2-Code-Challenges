3af821d853f63a38ffe8ddc19f8fd4d4bcc9ed2e
Frank Faustino <nawzyah@gmail.com>
2017-08-25 10:49:30 -0700

    Completed vowelCount.js

--- vowelCount/vowelCount.js
+++ vowelCount/vowelCount.js
@@ -6,23 +6,23 @@
 // Test Case
 const a = 'Hello World!'; // 3
 const b = ''; // 0
-const c = 'Disrupt jean shorts viral hella meh, plaid cupidatat magna art party.' // 20
+const c = 'Disrupt jean shorts viral hella meh, plaid cupidatat magna art pArty.' // 20
 
 // Solution using match() and RegEx
 const vowelCount = str => (str.match(/[aeiou]/gi) || []).length;
 
 // Solution using Array.from(), filter() and includes()
-const vowelCount = str => Array.from(str).filter(i => 'aeiou'.includes(i)).length;
+// const vowelCount = str => Array.from(str).filter(i => 'aeiouAEIOU'.includes(i)).length;
 
 // Sean's Solution using Set
-const vowelCount = str => {
-  const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.has(str[i])) count++;
-  }
-  return count;
-}
+// const vowelCount = str => {
+//   const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (vowels.has(str[i])) count++;
+//   }
+//   return count;
+// }
 
 console.log(vowelCount(a));
 console.log(vowelCount(b));
